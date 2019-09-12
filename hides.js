72aca8302328620ed21cfb97bb7b1d2aecc0e61b
reneDuarte <rene_duarte@live.com>
2019-09-12 17:34:20 -0300

    Update hides.js

--- hides.js
+++ hides.js
@@ -1,5 +1,5 @@
 document.querySelector('a').style.display = 'none';
-document.querySelector('.site-footer-credits').style.display = 'none';
+//document.querySelector('.site-footer-credits').style.display = 'none';
 
 
 
