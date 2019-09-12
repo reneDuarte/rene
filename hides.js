39e9aad0edc55364beac4ff7a698aaf8d700586c
reneDuarte <rene_duarte@live.com>
2019-09-12 17:55:05 -0300

    Update hides.js

--- hides.js
+++ hides.js
@@ -3,20 +3,19 @@ document.querySelector('a').style.display = 'none';
 function ocultar(id){
    if( document.querySelector(id)){
         document.querySelector(id).style.display = 'none'; 
-   }
-    
+   }    
     return  document.querySelector(id).style.display == 'none';
 }
 function serv(id){
   setTimeout(function(){  
       var ocultou = ocultar(id)
       if( !ocultou ){
-        serv();
+        servOculta(id);
       }
          
-    } , 3000);
-
+    } , 500);
 }
 
-serv('.site-footer-owner');
+servOculta('.site-footer-owner');
+servOculta('.site-footer-credits');
 
